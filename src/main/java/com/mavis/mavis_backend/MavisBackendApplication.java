c70a1825252dfba2c31064b759422ebc60124fc2
minjun <ijmtylhtf@naver.com>
2025-08-03 23:51:39 +0900

    initial commit

---
+++ src/main/java/com/mavis/mavis_backend/MavisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mavis.mavis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavisBackendApplication.class, args);
+	}
+
+}
