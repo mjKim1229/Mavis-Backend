43ed2da79524c4c8e61158352db367c226e43f0f
minjun <ijmtylhtf@naver.com>
2025-08-09 17:25:53 +0900

    feat : ErrorResponse 정의, CodeException 처리 (#2)

---
+++ Mavis-Api/src/main/java/com/mavis/api/config/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.mavis.api.config;
+
+import com.mavis.common.dto.ErrorReason;
+import com.mavis.common.dto.ErrorResponse;
+import com.mavis.common.exception.MavisCodeException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(exception = MavisCodeException.class)
+    public ResponseEntity<ErrorResponse> handleMavisCodeException(
+            MavisCodeException e, HttpServletRequest request
+    ) {
+        ErrorReason errorReason = e.getErrorReason();
+        ErrorResponse errorResponse = new ErrorResponse(errorReason, request.getRequestURI());
+        return ResponseEntity.status(
+                        HttpStatusCode.valueOf(errorReason.status())
+                )
+                .body(errorResponse);
+    }
+}
