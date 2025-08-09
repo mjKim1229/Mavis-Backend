f557d4ca1bf362687a02bd60b700302f43444f97
minjun <ijmtylhtf@naver.com>
2025-08-09 11:09:00 +0900

    feat : static 클래스 정의 (#1)

--- Mavis-Common/src/main/java/com/mavis/common/consts/MavisStatic.java
+++ Mavis-Common/src/main/java/com/mavis/common/consts/MavisStatic.java
@@ -1,4 +1,9 @@
-package man.java.com.mavis.common.consts;
+package com.mavis.common.consts;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MavisStatic {
+    public static final String BEARER = "Bearer ";
 }
