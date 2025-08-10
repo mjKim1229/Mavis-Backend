841c22094f74cd5b4477238434077f437dc99799
minjun <ijmtylhtf@naver.com>
2025-08-10 18:40:32 +0900

    refactor : 에러 로그 추가 (#2)

--- Mavis-Api/src/main/java/com/mavis/api/config/GlobalExceptionHandler.java
+++ Mavis-Api/src/main/java/com/mavis/api/config/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import com.mavis.common.dto.ErrorResponse;
 import com.mavis.common.exception.GlobalErrorCode;
 import com.mavis.common.exception.MavisCodeException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -29,6 +31,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(exception = Exception.class)
     public ResponseEntity<Object> handleAllException(Exception e) {
         GlobalErrorCode errorCode = GlobalErrorCode.INTERNAL_SERVER_ERROR;
+        log.error("INTERNAL_SERVER_ERROR", e);
         return ResponseEntity.status(errorCode.getStatus())
                 .body(errorCode.getErrorReason());
     }
