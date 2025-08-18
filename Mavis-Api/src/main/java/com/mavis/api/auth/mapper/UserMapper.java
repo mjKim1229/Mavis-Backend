bbda9aa01551443798ce4380cd8beeb63258bb3b
minjun <ijmtylhtf@naver.com>
2025-08-18 23:04:40 +0900

    feat : Naver 로그인 controller & service 뼈대 구현 (#12)

--- Mavis-Api/src/main/java/com/mavis/api/auth/mapper/UserMapper.java
+++ Mavis-Api/src/main/java/com/mavis/api/auth/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.mavis.api.auth.mapper;
 
+import com.mavis.common.properties.NaverProperties;
 import com.mavis.infrastructure.outer.api.oauth.dto.KakaoOAuthRequest;
 import com.mavis.common.properties.KakaoProperties;
+import com.mavis.infrastructure.outer.api.oauth.dto.NaverOAuthRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +11,22 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserMapper {
     private final KakaoProperties kakaoProperties;
+    private final NaverProperties naverProperties;
 
     public KakaoOAuthRequest fromCode(String code) {
         return KakaoOAuthRequest.builder()
                 .code(code)
                 .redirectUrl(kakaoProperties.redirectUrl())
                 .clientId(kakaoProperties.clientId())
+                .build();
+    }
+
+    public NaverOAuthRequest fromNaverCode(String code) {
+        return NaverOAuthRequest.builder()
+                .code(code)
+                .clientSecret(naverProperties.clientSecret())
+                .clientId(naverProperties.clientId())
+                .state(naverProperties.state())
                 .build();
     }
 }
