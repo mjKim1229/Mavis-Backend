c47777608f5558e97bae0d5d8cac5cb16c33140a
minjun <ijmtylhtf@naver.com>
2025-08-18 23:02:55 +0900

    feat : 네이버 환경 변수 주입 (#12)

--- Mavis-Common/src/main/java/com/mavis/common/config/ConfigurationPropertiesConfig.java
+++ Mavis-Common/src/main/java/com/mavis/common/config/ConfigurationPropertiesConfig.java
@@ -2,12 +2,14 @@ package com.mavis.common.config;
 
 import com.mavis.common.properties.JwtProperties;
 import com.mavis.common.properties.KakaoProperties;
+import com.mavis.common.properties.NaverProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties({
         KakaoProperties.class,
-        JwtProperties.class
+        JwtProperties.class,
+        NaverProperties.class
 })
 @Configuration
 public class ConfigurationPropertiesConfig {
