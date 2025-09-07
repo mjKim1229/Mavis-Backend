4a48372156db4b2c4444a361dbe8addffaffcdbc
minjun <ijmtylhtf@naver.com>
2025-09-07 16:50:55 +0900

    fix : format으로 변경 (#36)

--- Mavis-Api/src/main/java/com/mavis/api/inquiry/dto/GetProductInquiryResponse.java
+++ Mavis-Api/src/main/java/com/mavis/api/inquiry/dto/GetProductInquiryResponse.java
@@ -1,26 +1,30 @@
 package com.mavis.api.inquiry.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mavis.api.inquiry.domain.Inquiry;
 import lombok.Builder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 //TODO Admin, User 닉네임
 @Builder
 public record GetProductInquiryResponse(
         String question,
-        LocalDate questionCreatedAt,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        LocalDateTime questionCreatedAt,
         String answer,
-        LocalDate answerCreatedAt,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        LocalDateTime answerCreatedAt,
         String userNickname,
         boolean isPrivate
 ) {
     public static GetProductInquiryResponse from(Inquiry inquiry) {
         return GetProductInquiryResponse.builder()
                 .question(inquiry.getQuestion())
-                .questionCreatedAt(inquiry.getCreatedAt().toLocalDate())
+                .questionCreatedAt(inquiry.getCreatedAt())
                 .answer(inquiry.getAnswer())
-                .answerCreatedAt(inquiry.getAnsweredDatetime().toLocalDate())
+                .answerCreatedAt(inquiry.getAnsweredDatetime())
                 .isPrivate(inquiry.isPrivate())
                 .build();
     }
