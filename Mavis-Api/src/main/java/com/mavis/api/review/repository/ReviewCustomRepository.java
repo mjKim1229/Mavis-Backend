3a60c3d62077d59e172efa706409089fd0e48a75
minjun <ijmtylhtf@naver.com>
2025-09-11 23:58:25 +0900

    refactor : service에서 매핑으로 변경 (#42)

--- Mavis-Api/src/main/java/com/mavis/api/review/repository/ReviewCustomRepository.java
+++ Mavis-Api/src/main/java/com/mavis/api/review/repository/ReviewCustomRepository.java
@@ -1,5 +1,6 @@
 package com.mavis.api.review.repository;
 
+import com.mavis.api.review.domain.Review;
 import com.mavis.api.review.dto.ProductReviewTotal;
 import com.mavis.api.review.dto.ReviewResponse;
 import org.springframework.data.domain.Page;
@@ -8,5 +9,5 @@ import org.springframework.data.domain.Pageable;
 public interface ReviewCustomRepository {
     ProductReviewTotal queryProductReviewTotal(Long productId);
 
-    Page<ReviewResponse> queryProductReviews(Long productId, Pageable pageable);
+    Page<Review> queryProductReviews(Long productId, Pageable pageable);
 }
--- Mavis-Api/src/main/java/com/mavis/api/review/service/ReviewService.java
+++ Mavis-Api/src/main/java/com/mavis/api/review/service/ReviewService.java
@@ -2,6 +2,7 @@ package com.mavis.api.review.service;
 
 import com.mavis.api.common.page.PageResponse;
 import com.mavis.api.review.domain.Review;
+import com.mavis.api.review.domain.ReviewImage;
 import com.mavis.api.review.dto.CreateReviewRequest;
 import com.mavis.api.review.dto.ProductReviewTotal;
 import com.mavis.api.review.dto.ReviewResponse;
@@ -11,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +35,15 @@ public class ReviewService {
 
     @Transactional(readOnly = true)
     public PageResponse<ReviewResponse> getProductReviews(Long productId, Pageable pageable) {
-        Page<ReviewResponse> reviewResponses = reviewRepository.queryProductReviews(productId, pageable);
-        return PageResponse.of(reviewResponses);
+        Page<ReviewResponse> reviewPages = reviewRepository.queryProductReviews(productId, pageable)
+                .map(review -> {
+                    List<String> reviewImages = review.getImages()
+                            .stream()
+                            .map(ReviewImage::getImageUrl)
+                            .toList();
+                    return ReviewResponse.of(review, reviewImages);
+                });
+
+        return PageResponse.of(reviewPages);
     }
 }
