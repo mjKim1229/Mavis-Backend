76b9b883379d590678c801c27504e16e9e71ed76
minjun <ijmtylhtf@naver.com>
2025-09-12 00:12:14 +0900

    chore : import 제거 (#42)

--- Mavis-Api/src/main/java/com/mavis/api/review/repository/ReviewCustomRepository.java
+++ Mavis-Api/src/main/java/com/mavis/api/review/repository/ReviewCustomRepository.java
@@ -2,7 +2,6 @@ package com.mavis.api.review.repository;
 
 import com.mavis.api.review.domain.Review;
 import com.mavis.api.review.dto.ProductReviewTotal;
-import com.mavis.api.review.dto.ReviewResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
