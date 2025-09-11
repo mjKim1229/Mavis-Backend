50eb224977eef0aa88140661348405a81868d92e
minjun <ijmtylhtf@naver.com>
2025-09-11 22:19:12 +0900

    refactor : entity 추가 (#42)

--- Mavis-Api/src/main/java/com/mavis/api/review/dto/ReviewResponse.java
+++ Mavis-Api/src/main/java/com/mavis/api/review/dto/ReviewResponse.java
@@ -1,11 +1,13 @@
 package com.mavis.api.review.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record ReviewResponse(
         Long reviewId,
         int score,
         LocalDateTime createdAt,
-        int quantity
+        int quantity,
+        List<String> imageUrls
 ) {
 }
