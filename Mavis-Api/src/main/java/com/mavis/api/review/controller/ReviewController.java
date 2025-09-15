70bcb571b9f34ef2bcd4c0376ac421d37fa95da2
minjun <ijmtylhtf@naver.com>
2025-09-15 19:56:22 +0900

    feat : 리뷰 이미지 요청 주가 (#44)

--- Mavis-Api/src/main/java/com/mavis/api/review/controller/ReviewController.java
+++ Mavis-Api/src/main/java/com/mavis/api/review/controller/ReviewController.java
@@ -8,7 +8,11 @@ import com.mavis.api.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +21,9 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @PostMapping
-    public void createReview(@RequestBody CreateReviewRequest request) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    public void createReview(@RequestPart CreateReviewRequest request,
+                             @RequestPart List<MultipartFile> images) {
         reviewService.createReview(request);
     }
 
