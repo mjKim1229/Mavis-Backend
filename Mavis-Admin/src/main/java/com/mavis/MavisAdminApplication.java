b864e020af5b62c1863bf237412cbc008ce0f9c7
minjun <ijmtylhtf@naver.com>
2025-09-16 20:24:46 +0900

    refactor : admin 모듈 분리 (#49)

---
+++ Mavis-Admin/src/main/java/com/mavis/MavisAdminApplication.java
@@ -0,0 +1,12 @@
+package com.mavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavisAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MavisAdminApplication.class);
+    }
+}
+
