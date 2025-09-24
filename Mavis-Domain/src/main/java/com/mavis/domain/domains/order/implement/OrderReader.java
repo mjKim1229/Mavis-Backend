b2f4879153382f6c957267f10eb93ebb62eb92a9
minjun <ijmtylhtf@naver.com>
2025-09-24 20:51:49 +0900

    refactor : order item repo 추가 (#78)

--- Mavis-Domain/src/main/java/com/mavis/domain/domains/order/implement/OrderReader.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/order/implement/OrderReader.java
@@ -2,6 +2,7 @@ package com.mavis.domain.domains.order.implement;
 
 import com.mavis.domain.domains.order.domain.OrderItem;
 import com.mavis.domain.domains.order.exception.OrderNotFoundException;
+import com.mavis.domain.domains.order.repository.OrderItemRepository;
 import com.mavis.domain.domains.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,9 +12,10 @@ import org.springframework.stereotype.Component;
 public class OrderReader {
 
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
 
     public OrderItem findById(Long id) {
-        return orderRepository.findById(id)
+        return orderItemRepository.findByIdAndIsDeletedFalse(id)
                 .orElseThrow(() -> OrderNotFoundException.EXCEPTION);
     }
 }
---
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/order/repository/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.mavis.domain.domains.order.repository;
+
+import com.mavis.domain.domains.order.domain.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Optional<OrderItem> findByIdAndIsDeletedFalse(Long id);
+}
