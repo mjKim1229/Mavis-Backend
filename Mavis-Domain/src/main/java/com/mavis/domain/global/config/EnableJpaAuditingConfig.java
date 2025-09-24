96a14d18e65d1e65cc41c3066dcb188b3e17b8b1
minjun <ijmtylhtf@naver.com>
2025-09-24 22:15:15 +0900

    fix : baseentity 수정 (#78)

--- Mavis-Domain/src/main/java/com/mavis/domain/global/config/EnableJpaAuditingConfig.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/global/config/EnableJpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.mavis.api.config;
+package com.mavis.domain.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
