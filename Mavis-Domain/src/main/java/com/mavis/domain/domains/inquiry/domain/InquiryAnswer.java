770e882c062c41cfe8a671e487feaac80091cea1
minjun <ijmtylhtf@naver.com>
2025-09-25 20:54:27 +0900

    refactor : inquiry answer 분리 (#84)

--- Mavis-Domain/src/main/java/com/mavis/domain/domains/inquiry/domain/InquiryAnswer.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/inquiry/domain/InquiryAnswer.java
@@ -1,35 +1,25 @@
 package com.mavis.domain.domains.inquiry.domain;
 
 import com.mavis.domain.domains.common.jpa.BaseEntity;
-import com.mavis.domain.domains.product.domain.Product;
-import com.mavis.domain.domains.user.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Builder
 @AllArgsConstructor
-public class Inquiry extends BaseEntity {
+public class InquiryAnswer extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    private String question;
+    private String answer;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    private Product product;
-
-    private boolean isPrivate;
+    @JoinColumn(name = "inquiry_id")
+    private Inquiry inquiry;
 
     @Builder.Default
     private boolean isDeleted = false;
