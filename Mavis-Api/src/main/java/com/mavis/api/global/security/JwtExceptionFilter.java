a8c7bb54c66f148abc74f332e48a409a5a16ffda
minjun <ijmtylhtf@naver.com>
2025-09-30 23:17:29 +0900

    feat : jwt exception 필터 (#89)

---
+++ Mavis-Api/src/main/java/com/mavis/api/global/security/JwtExceptionFilter.java
@@ -0,0 +1,46 @@
+package com.mavis.api.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mavis.common.dto.ErrorResponse;
+import com.mavis.common.exception.MavisCodeException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (MavisCodeException e) {
+			responseToClient(response,
+				getErrorResponse(e, request.getRequestURI().toString()));
+		}
+	}
+
+	private void responseToClient(HttpServletResponse response, ErrorResponse errorResponse)
+		throws IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(errorResponse.getStatus());
+		response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+	}
+
+	private ErrorResponse getErrorResponse(MavisCodeException e, String path) {
+		return new ErrorResponse(e.getErrorReason(), path);
+	}
+}
