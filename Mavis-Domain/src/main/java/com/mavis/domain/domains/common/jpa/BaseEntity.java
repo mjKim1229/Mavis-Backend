faca01b3f4b7a76110229994ec26b494f103ad05
minjun <ijmtylhtf@naver.com>
2025-10-06 15:12:27 +0900

    fix : 수정 (#107)

--- Mavis-Domain/src/main/java/com/mavis/domain/domains/common/jpa/BaseEntity.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/common/jpa/BaseEntity.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -20,6 +20,6 @@ public abstract class BaseEntity {
     private LocalDateTime createdAt;
 
     @Column
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
