595749ec5a4718c69cd66e56e660f9c606cbb021
minjun <ijmtylhtf@naver.com>
2025-10-13 00:09:30 +0900

    chore : 원복 (#123)

--- Mavis-Admin/src/main/java/com/mavis/admin/domains/inquiry/controller/InquiryAnswerController.java
+++ Mavis-Admin/src/main/java/com/mavis/admin/domains/inquiry/controller/InquiryAnswerController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class InquiryAnswerController {
 
     private final InquiryAnswerService inquiryAnswerService;
-
-    @CrossOrigin
+    
     @PostMapping("/{inquiryId}")
     public void createInquiryAnswer(@PathVariable Long inquiryId, @RequestBody CreateInquiryAnswerRequest request) {
         inquiryAnswerService.createInquiryAnswer(inquiryId, request.answer());
