e1523790782028a1a85733996d8a4ca1b7fd74b8
minjun <ijmtylhtf@naver.com>
2025-10-14 23:23:49 +0900

    chore : / 제외 (#123)

--- Mavis-Admin/src/main/java/com/mavis/admin/global/config/SecurityConfig.java
+++ Mavis-Admin/src/main/java/com/mavis/admin/global/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000/"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
