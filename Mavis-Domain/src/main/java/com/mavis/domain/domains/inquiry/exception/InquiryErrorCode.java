6929e34cbaf95f11e1dfc8bc5b686aaee68433c4
minjun <ijmtylhtf@naver.com>
2025-10-29 21:11:27 +0900

    fix : 예외처리 추가 (#138)

--- Mavis-Domain/src/main/java/com/mavis/domain/domains/inquiry/exception/InquiryErrorCode.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/inquiry/exception/InquiryErrorCode.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum InquiryErrorCode implements BaseErrorCode {
-    INQUIRY_NOT_FOUND(404, "존재하지 않는 문의사항입니다.", "INQUIRY_404_1");
+    INQUIRY_NOT_FOUND(404, "존재하지 않는 문의사항입니다.", "INQUIRY_404_1"),
+    INQUIRY_ALREADY_ANSWERED(400, "이미 답변한 문의사항입니다.", "INQUIRY_400_1");
     private final Integer status;
     private final String message;
     private final String code;
