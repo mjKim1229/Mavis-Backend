5bde0ff36775cd1fef9ddbf02cee8e3562e5b4fd
minjun <ijmtylhtf@naver.com>
2025-11-22 17:03:50 +0900

    feat : color 수정 코드 (#160)

--- Mavis-Admin/src/main/java/com/mavis/admin/domains/product/service/AdminProductService.java
+++ Mavis-Admin/src/main/java/com/mavis/admin/domains/product/service/AdminProductService.java
@@ -8,6 +8,8 @@ import com.mavis.domain.domains.product.domain.ProductColor;
 import com.mavis.domain.domains.product.domain.ProductImage;
 import com.mavis.domain.domains.product.domain.ProductNotice;
 import com.mavis.domain.domains.product.implement.ProductReader;
+import com.mavis.domain.domains.product.repository.ProductColorRepository;
+import com.mavis.domain.domains.product.repository.ProductImageRepository;
 import com.mavis.domain.domains.product.repository.ProductNoticeRepository;
 import com.mavis.domain.domains.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +29,7 @@ public class AdminProductService {
     private final ProductNoticeRepository productNoticeRepository;
     private final ProductImageAppender productImageAppender;
     private final ProductReader productReader;
+    private final ProductColorRepository productColorRepository;
 
     @Transactional(readOnly = true)
     public List<GetProductResponse> getProductList(Pageable pageable) {
@@ -69,12 +72,29 @@ public class AdminProductService {
 
     @Transactional
     public void updateProduct(Long productId, UpdateProductRequest request) {
+        //product
         Product product = productReader.readById(productId);
         product.update(request.name(), request.price(), request.subCategory());
 
+        //notice
         ProductNotice productNotice = product.getProductNotice();
         ProductNoticeVO notice = request.notice();
         productNotice.update(notice.precaution(), notice.shippingInfo(), notice.returnRequest(), notice.returnProcess());
+
+        //color
+        List<String> requestColors = request.colors();
+        List<String> recentColors = extractColors(product);
+
+        List<String> toAddColors = requestColors.stream()
+                .filter(color -> !recentColors.contains(color))
+                .toList();
+        productColorAppender.saveProductColors(toAddColors, product);
+
+        List<ProductColor> recentProductColors = product.getColors();
+        List<ProductColor> toDeleteColors = recentProductColors.stream()
+                .filter(color -> !requestColors.contains(color.getColor()))
+                .toList();
+        productColorRepository.deleteAll(toDeleteColors);
     }
 
     @Transactional
