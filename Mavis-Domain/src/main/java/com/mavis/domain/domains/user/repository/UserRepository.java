76ed331145a45136562003ebdf94c6df4ffe07f0
minjun <ijmtylhtf@naver.com>
2025-12-25 13:09:39 +0900

    refactor : 회원 정보 없을때 성공 응답 처리 & 메일 미전송

--- Mavis-Domain/src/main/java/com/mavis/domain/domains/user/repository/UserRepository.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/user/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameAndIsDeletedFalse(String username);
     Optional<User> findBySnsTypeAndSnsIdAndIsDeletedFalse(SnsType snsType, String snsId);
+    boolean existsByEmailAndIsDeletedFalse(String email);
 }
