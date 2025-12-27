d17b00ee6d1122045368060aa512c9b09512909a
minjun <ijmtylhtf@naver.com>
2025-12-27 21:42:10 +0900

    feat : logging filter 추가 (#216)

--- Mavis-Admin/src/main/java/com/mavis/admin/global/config/SecurityConfig.java
+++ Mavis-Admin/src/main/java/com/mavis/admin/global/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.mavis.admin.global.security.FilterConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
