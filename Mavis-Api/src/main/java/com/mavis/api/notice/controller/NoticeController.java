e77b6fa344869fc2112c98ea116023b30fdf9e91
minjun <ijmtylhtf@naver.com>
2026-02-16 01:03:24 +0900

    chore : notice swagger 추가

--- Mavis-Api/src/main/java/com/mavis/api/notice/controller/NoticeController.java
+++ Mavis-Api/src/main/java/com/mavis/api/notice/controller/NoticeController.java
@@ -4,6 +4,8 @@ import com.mavis.api.common.page.PageResponse;
 import com.mavis.api.notice.dto.NoticeResponse;
 import com.mavis.api.notice.dto.NoticeSummaryResponse;
 import com.mavis.api.notice.service.NoticeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
@@ -15,16 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/api/notice")
+@Tag(name = "공지사항 API")
 public class NoticeController {
 
     private final NoticeService noticeService;
 
     @GetMapping("/{noticeId}")
+    @Operation(summary = "공지사항 상세 조회")
     public NoticeResponse getNotice(@PathVariable Long noticeId) {
         return noticeService.getNotice(noticeId);
     }
 
     @GetMapping
+    @Operation(summary = "공지사항 목록 조회")
     public PageResponse<NoticeSummaryResponse> getNotices(@ParameterObject Pageable pageable) {
         return noticeService.getNotices(pageable);
     }
