96eb171bab558e1b46448f3879fe08a03c3f996c
minjun <ijmtylhtf@naver.com>
2026-02-22 23:25:27 +0900

    fix : 한글 인코딩

--- Mavis-Admin/src/main/java/com/mavis/admin/domains/delivery/controller/AdminDeliveryController.java
+++ Mavis-Admin/src/main/java/com/mavis/admin/domains/delivery/controller/AdminDeliveryController.java
@@ -11,6 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping("/v1/api/delivery")
 @RequiredArgsConstructor
@@ -21,7 +24,21 @@ public class AdminDeliveryController {
     @Operation(summary = "발주 완료 주문 목록 엑셀 다운로드")
     @GetMapping("/excel")
     public ResponseEntity<byte[]> getOrdersByExcel(Pageable pageable) {
+
         byte[] orderBytes = adminDeliveryService.getOrderByExcel(pageable);
+
+        String fileName = "배송목록.xlsx";
+
+        String encodedFileName = URLEncoder.encode(
+                fileName,
+                StandardCharsets.UTF_8
+        ).replaceAll("\\+", "%20");
+
+        String contentDisposition =
+                "attachment; " +
+                        "filename=\"delivery.xlsx\"; " +
+                        "filename*=UTF-8''" + encodedFileName;
+
         return ResponseEntity.ok()
                 .contentType(
                         MediaType.parseMediaType(
@@ -30,10 +47,11 @@ public class AdminDeliveryController {
                 )
                 .header(
                         HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment; filename=배송목록.xlsx"
+                        contentDisposition
                 )
                 .body(orderBytes);
     }
+
 
     @Operation(summary = "주문 발주 확인 & 배송 생성")
     @PostMapping("/confirm")
