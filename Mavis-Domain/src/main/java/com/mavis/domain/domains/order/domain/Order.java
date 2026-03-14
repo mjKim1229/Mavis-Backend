e71acc1a56f1390326f025d934547bec0d250b36
minjun <ijmtylhtf@naver.com>
2026-03-14 15:28:48 +0900

    refactor : 주문 번호 unique 제약 추가

--- Mavis-Domain/src/main/java/com/mavis/domain/domains/order/domain/Order.java
+++ Mavis-Domain/src/main/java/com/mavis/domain/domains/order/domain/Order.java
@@ -18,6 +18,7 @@ public class Order extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String orderId;
 
     @ManyToOne(fetch = FetchType.LAZY)
