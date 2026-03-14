7228f66b90b56b7e4bc849d2784c501229a6aa81
minjun <ijmtylhtf@naver.com>
2026-03-14 15:49:52 +0900

    refactor : 프런트 요청에서 받은 orderid 삭제

--- Mavis-Api/src/main/java/com/mavis/api/order/dto/CreateOrderRequest.java
+++ Mavis-Api/src/main/java/com/mavis/api/order/dto/CreateOrderRequest.java
@@ -3,7 +3,6 @@ package com.mavis.api.order.dto;
 import java.util.List;
 
 public record CreateOrderRequest(
-        String orderId,
         int amount,
         OrderAddressRequest orderAddressRequest,
         List<OrderProduct> orderItems
