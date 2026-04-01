69f8d6e3aef3239f380fdf5fd13efd9b0fe7c726
minjun <ijmtylhtf@naver.com>
2026-04-02 00:36:36 +0900

    chore : dto 수정

--- Mavis-Admin/src/main/java/com/mavis/admin/domains/refund/dto/GetAdminRefundResponse.java
+++ Mavis-Admin/src/main/java/com/mavis/admin/domains/refund/dto/GetAdminRefundResponse.java
@@ -1,5 +1,7 @@
 package com.mavis.admin.domains.refund.dto;
 
+import com.mavis.domain.domains.order.domain.Order;
+import com.mavis.domain.domains.order.domain.OrderItem;
 import com.mavis.domain.domains.refund.domain.Refund;
 import com.mavis.domain.domains.refund.domain.RefundStatus;
 import lombok.Builder;
@@ -7,13 +9,15 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.mavis.common.util.OrderNumberGenerator.DOMAIN_PREFIX;
+
 @Builder
 public record GetAdminRefundResponse(
         Long refundId,
         Long orderItemId,
+        String tossOrderId,
         String productName,
         String color,
-        int orderQuantity,
         int refundQuantity,
         int refundAmount,
         String refundReason,
@@ -24,18 +28,19 @@ public record GetAdminRefundResponse(
         LocalDateTime requestedAt
 ) {
     public static GetAdminRefundResponse from(Refund refund) {
-        var orderItem = refund.getOrderItem();
+        OrderItem orderItem = refund.getOrderItem();
+        Order order = orderItem.getOrder();
         return GetAdminRefundResponse.builder()
                 .refundId(refund.getId())
                 .orderItemId(orderItem.getId())
+                .tossOrderId(order.getOrderId().substring(DOMAIN_PREFIX.length()))
                 .productName(orderItem.getProduct().getName())
                 .color(orderItem.getColor())
-                .orderQuantity(orderItem.getQuantity())
                 .refundQuantity(refund.getRefundQuantity())
                 .refundAmount(refund.getRefundAmount())
                 .refundReason(refund.getRefundReason())
                 .refundStatus(refund.getRefundStatus())
-                .userName(orderItem.getOrder().getUser().getName())
+                .userName(order.getUser().getName())
                 .trackingNumber(refund.getTrackingNumber())
                 .imageUrls(refund.getImages().stream().map(image -> image.getImageUrl()).toList())
                 .requestedAt(refund.getCreatedAt())
