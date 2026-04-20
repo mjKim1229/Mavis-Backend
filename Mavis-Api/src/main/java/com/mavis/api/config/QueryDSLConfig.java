66758923c6cd529bdc83dd1bc4b6405069d4b8c3
minjun <ijmtylhtf@naver.com>
2026-04-20 23:40:51 +0900

    fix : qclass deadlock

--- Mavis-Api/src/main/java/com/mavis/api/config/QueryDSLConfig.java
+++ Mavis-Api/src/main/java/com/mavis/api/config/QueryDSLConfig.java
@@ -1,11 +1,18 @@
 package com.mavis.api.config;
 
+import com.mavis.domain.domains.delivery.domain.QDelivery;
+import com.mavis.domain.domains.order.domain.QOrderItem;
+import com.mavis.domain.domains.product.domain.QProduct;
+import com.mavis.domain.domains.product.domain.QProductImage;
+import com.mavis.domain.domains.review.domain.QReview;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class QueryDSLConfig {
 
@@ -14,6 +21,13 @@ public class QueryDSLConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
+        QReview qReview = QReview.review;
+        QOrderItem qOrderItem = QOrderItem.orderItem;
+        QProduct qProduct = QProduct.product;
+        QProductImage qProductImage = QProductImage.productImage;
+        QDelivery qDelivery = QDelivery.delivery;
+        log.debug("QueryDSLConfig initialized with QReview: {}, QOrderItem: {}, QProduct: {}, QProductImage: {}, QDelivery: {}",);
+                qReview, qOrderItem, qProduct, qProductImage, qDelivery);
         return new JPAQueryFactory(entityManager);
     }
 }
