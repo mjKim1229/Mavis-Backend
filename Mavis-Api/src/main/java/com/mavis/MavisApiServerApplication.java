faa0158f8dcc5a31a6db2ddcd245fac91502f0f6
minjun <ijmtylhtf@naver.com>
2026-04-21 00:35:30 +0900

    fix : 로깅

--- Mavis-Api/src/main/java/com/mavis/MavisApiServerApplication.java
+++ Mavis-Api/src/main/java/com/mavis/MavisApiServerApplication.java
@@ -8,12 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MavisApiServerApplication {
     public static void main(String[] args) {
+        System.out.println(">>>> [STEP 1] START LOADING QREVIEW");
         try {
+            // 이 라인을 넘어가야 다음 로그가 찍힙니다.
             Class.forName("com.mavis.domain.domains.review.domain.QReview");
+            System.out.println(">>>> [STEP 2] QREVIEW LOADED");
+
             Class.forName("com.mavis.domain.domains.order.domain.QOrderItem");
-            log.info("QCLASS LOADING DONE");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println(">>>> [STEP 3] QORDERITEM LOADED");
+
+            // 여기까지 와야 안전한 겁니다.
+            System.out.println(">>>> [SUCCESS] ALL QCLASSES READY");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         SpringApplication.run(MavisApiServerApplication.class);
