7bc15fd54aa54bb15f787d8237d6c391791f85ec
minjun <ijmtylhtf@naver.com>
2026-04-21 00:25:48 +0900

    fix : main에서 초기화

--- Mavis-Api/src/main/java/com/mavis/MavisApiServerApplication.java
+++ Mavis-Api/src/main/java/com/mavis/MavisApiServerApplication.java
@@ -1,11 +1,21 @@
 package com.mavis;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MavisApiServerApplication {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mavis.domain.domains.review.domain.QReview");
+            Class.forName("com.mavis.domain.domains.order.domain.QOrderItem");
+            log.info("QCLASS LOADING DONE");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         SpringApplication.run(MavisApiServerApplication.class);
     }
 }
