5d567e86d8cb8a12891b106e50843b82f67a93e5
minjun <ijmtylhtf@naver.com>
2026-04-25 22:54:37 +0900

    chore : Q Initialize 기존 작업 정리

--- Mavis-Api/src/main/java/com/mavis/MavisApiServerApplication.java
+++ Mavis-Api/src/main/java/com/mavis/MavisApiServerApplication.java
@@ -1,28 +1,11 @@
 package com.mavis;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class MavisApiServerApplication {
     public static void main(String[] args) {
-        System.out.println(">>>> [STEP 1] START LOADING QREVIEW");
-        try {
-            // 이 라인을 넘어가야 다음 로그가 찍힙니다.
-            Class.forName("com.mavis.domain.domains.review.domain.QReview");
-            System.out.println(">>>> [STEP 2] QREVIEW LOADED");
-
-            Class.forName("com.mavis.domain.domains.order.domain.QOrderItem");
-            System.out.println(">>>> [STEP 3] QORDERITEM LOADED");
-
-            // 여기까지 와야 안전한 겁니다.
-            System.out.println(">>>> [SUCCESS] ALL QCLASSES READY");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        SpringApplication.run(MavisApiServerApplication.class);
+        SpringApplication.run(MavisApiServerApplication.class, args);
     }
 }
--- Mavis-Api/src/main/java/com/mavis/api/config/QueryDSLConfig.java
+++ Mavis-Api/src/main/java/com/mavis/api/config/QueryDSLConfig.java
@@ -1,18 +1,11 @@
 package com.mavis.api.config;
 
-import com.mavis.domain.domains.delivery.domain.QDelivery;
-import com.mavis.domain.domains.order.domain.QOrderItem;
-import com.mavis.domain.domains.product.domain.QProduct;
-import com.mavis.domain.domains.product.domain.QProductImage;
-import com.mavis.domain.domains.review.domain.QReview;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class QueryDSLConfig {
 
@@ -21,12 +14,6 @@ public class QueryDSLConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
-//        QReview qReview = QReview.review;
-//        QOrderItem qOrderItem = QOrderItem.orderItem;
-//        QProduct qProduct = QProduct.product;
-//        QProductImage qProductImage = QProductImage.productImage;
-//        QDelivery qDelivery = QDelivery.delivery;
-//        log.debug("QueryDSLConfig initialized");
         return new JPAQueryFactory(entityManager);
     }
 }
